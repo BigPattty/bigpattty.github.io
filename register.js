8f4767e185c1602785b7d6c8fdd6ea3082cd1a73
BigPattty <86346146+BigPattty@users.noreply.github.com>
2024-06-09 19:53:57 +1000

    Update register.js

--- register.js
+++ register.js
@@ -1,3 +1,5 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 document.addEventListener('DOMContentLoaded', function () {
     const registerForm = document.getElementById('registerForm');
     const messageDiv = document.getElementById('message');
