aaef1bbb916dac355111bccd160fa39ea1870908
BigPattty <86346146+BigPattty@users.noreply.github.com>
2024-06-09 19:22:07 +1000

    Update register.js

--- register.js
+++ register.js
@@ -15,7 +15,7 @@ document.getElementById('registerForm').addEventListener('submit', async (event)
   }
 
   try {
-    const response = await fetch('https://sddmajor.bh-games.com:27045/register', { // Use HTTPS and correct port
+    const response = await fetch('http://sddmajor.bh-games.com:27045/register', { // Use HTTPS and correct port
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
