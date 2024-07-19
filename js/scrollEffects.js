95f50b395d6b3f0b98883e13992b61b71366bd75
BigPattty <86346146+BigPattty@users.noreply.github.com>
2024-07-19 04:16:30 +0000

    fixing stuff

--- js/scrollEffects.js
+++ js/scrollEffects.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     const contentBoxes = document.querySelectorAll('.content-box');
-    const months = document.querySelectorAll('.month');
     
     const onScroll = () => {
         const scrollPos = window.scrollY + window.innerHeight;
@@ -8,12 +7,6 @@ document.addEventListener('DOMContentLoaded', () => {
         contentBoxes.forEach(box => {
             if (scrollPos > box.offsetTop + box.offsetHeight / 2) {
                 box.classList.add('visible');
-            }
-        });
-
-        months.forEach(month => {
-            if (scrollPos > month.offsetTop + month.offsetHeight / 2) {
-                month.classList.add('visible');
             }
         });
     };
